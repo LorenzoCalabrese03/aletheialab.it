5905ae8cfc4e257c9d6e7d19ca8e2e55e1e83363
LorenzoCalabrese03 <151526103+LorenzoCalabrese03@users.noreply.github.com>
2025-05-21 13:18:14 +0200

    Organigramma, ApiClient, correzione footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+"use client"
 import CodingInProgress from "@/components/CodingInPrograss/CodingInProgress";
 import Hero from "@/components/HomePage/Hero";
 import About from "@/components/HomePage/About";
-import Organigramma from "@/components/HomePage/Organigramma";
+import {Organigramma} from "@/components/HomePage/Organigramma";
+import {AnimatedSection} from "@/components/AnimatedSection";
+import {motion} from "framer-motion";
 
 export default function HomePage(){
   return(
@@ -17,9 +20,18 @@ export default function HomePage(){
           </section>
 
         {/*Organigramma*/}
-          <section>
-            {/*<Organigramma/>*/}
-          </section>
+          <AnimatedSection id={"organigramma"}
+                           delay={0.15}
+                           className="bg-background section-transition"
+                           style={{
+                               height: 200
+                           }}>
+              <div className="container px-4 md:px-6 relative z-10">
+                  <div>
+                      <Organigramma/>
+                  </div>
+              </div>
+          </AnimatedSection>
 
         {/*Service section*/}
           <section>
@@ -57,7 +69,7 @@ export default function HomePage(){
           </section>
 
           {/**/}
-        <CodingInProgress/>
+        {/*<CodingInProgress/>*/}
       </>
 
   );
--- components/HomePage/Hero.tsx
+++ components/HomePage/Hero.tsx
@@ -1,11 +1,11 @@
 "use client"
 import { motion } from "framer-motion";
 
-const MotionSection = motion("section");
+
 
 export default function Hero() {
     return (
-        <MotionSection
+        <motion.section
             id="hero"
             className="w-full py-32 md:py-40 lg:py-48 xl:py-56 relative overflow-hidden bg-background section-transition"
             initial={{ opacity: 0 }}
@@ -15,6 +15,6 @@ export default function Hero() {
             <div>
                 Contenuto Hero
             </div>
-        </MotionSection>
+        </motion.section>
     );
 }
